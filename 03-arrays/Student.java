e01d501209b445e2d8513e56bcf76162a403b83b
arnevandoorslaer@github.com <root@arne-vandoorslaer>
2020-03-03 11:57:58 +0100

    New question from category 03-arrays

---
+++ 03-arrays/Student.java
@@ -0,0 +1,4 @@
+public class Student {
+    // Schrijf een functie last(int[] xs) die gegeven een array xs het laatste element ervan teruggeeft.
+    // Je mag ervan uitgaan dat xs minstens één element bevat.
+}

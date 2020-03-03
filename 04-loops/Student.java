032edafb5513a453b618140c9b54fbaae46f1ad4
arnevandoorslaer@github.com <root@arne-vandoorslaer>
2020-03-03 11:58:13 +0100

    New question from category 04-loops

---
+++ 04-loops/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    // LOOPS ==============================================
+
+    // Je belegt €1000 tegen 5%. Schrijf een functie invest(goal) die gegeven een doelbedrag goal,
+    // uitrekent hoeveel jaar het duur eer je €1000 gegroeid zijn tot goal.
+    // Ga ervan uit dat de intrest telkens toegevoegd wordt op het einde van elk jaar.
+    //
+    // Je kan dit in principe met logaritmes oplossen, maar dit valt buiten het bereik van dit vak.
+    // Voor deze oefening is het de bedoeling dat je een lus gebruikt om herhaaldelijk 5% toe te voegen
+    // aan het initieel bedrag tot het voldoende gegroeid is.
+
+}

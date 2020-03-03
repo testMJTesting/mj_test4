79a070e636cf29947ae154e7d0b790f38a229dfd
arnevandoorslaer@github.com <root@arne-vandoorslaer>
2020-03-03 11:57:39 +0100

    New question from category 02-conditionals

---
+++ 02-conditionals/Student.java
@@ -0,0 +1,6 @@
+public class Student {
+    // Je wil t-shirts laten drukken.
+    // Per t-shirt kost het €10.
+    // Als je er meer dan 100 bestelt, krijg je korting en kost het slechts €9 per t-shirt.
+    // Als je er meer dan 500 bestelt, krijg je korting en kost het slechts €8 per t-shirt.
+}
